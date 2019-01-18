af5d468574fc0e267f9717a2f2c8b922c8878986
Михаил Державин <mderzhavin@ad.local>
2019-01-18 09:21:03 +0300

    commit

--- src/main/java/Controller/FileControl.java
+++ src/main/java/Controller/FileControl.java
@@ -5,8 +5,6 @@ import Model.Check;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.*;
 import java.util.ArrayList;
